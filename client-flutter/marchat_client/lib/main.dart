f911bb60f62f6b4d5f7e8122e1268768b46e6535
xXMuffinXx <hannes.weiss.06@gmx.de>
2020-06-18 21:01:45 +0200

    windows support :D

--- client-flutter/marchat_client/lib/main.dart
+++ client-flutter/marchat_client/lib/main.dart
@@ -12,6 +12,7 @@ class MarchatApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: marchatTheme,
       home: MainPage(),
